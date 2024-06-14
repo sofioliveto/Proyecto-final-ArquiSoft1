6b512806764fcf4dd18543a35802df9e409e288b
margarita0912 <2202199@ucc.edu.ar>
2024-06-13 23:24:29 -0300

    detalles de cursos front

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -7,6 +7,23 @@ import React from "react";
 
 const Item = ({ course }) => {
 
+    const formattedDate = new Date(course.fecha_inicio).toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    });
+
+    const getProfesorName = (profesor_id) => {
+        const profesores = {
+            2: 'Juan Lopez',
+            4: 'Margarita de Marcos',
+            8: 'Gustavo Jacobo',
+            17: 'Rodolfo Perez',
+            18: 'Sebastian Colidio',
+            19: 'Lucas Beltran'
+        };
+        return profesores[profesor_id] || 'Profesor desconocido';
+    };
 
     return (
         <Card
@@ -16,7 +33,7 @@ const Item = ({ course }) => {
         >
             <Image
                 objectFit='cover'
-                maxW={{ base: '100%', sm: '200px' }}
+                maxW={{sm: '250px'}}
                 src={course.url_image}
                 alt='Imagen Curso'git
             />
@@ -31,9 +48,21 @@ const Item = ({ course }) => {
                     <Text py='2' className="card-text">
                         {course.categoria}
                     </Text>
-                    <Text py='2' display='flex' alignItems='center' className="card-text">
+                    <Text marginBottom='3px' display='flex' py='2' alignItems='center' className="card-text">
                         <img src="/estrella.png" alt="estrella" width="20px" height="20px" style={{ marginRight: '5px' }}/>
                         {course.valoracion}/5
+                    </Text>
+                    <Text className="card-textt">
+                        Duracion: {course.duracion}hs
+                    </Text>
+                    <Text className="card-textt">
+                        Fecha de inicio: {formattedDate}
+                    </Text>
+                    <Text className="card-textt">
+                        Requisito: Nivel {course.requisitos}
+                    </Text>
+                    <Text className="card-textt">
+                        Profesor: {getProfesorName(course.profesor_id)}
                     </Text>
                 </CardBody>
                 <CardFooter>
