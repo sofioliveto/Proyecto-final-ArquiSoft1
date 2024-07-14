50b530d4a5da01a5aeafec5a8461be18cde41382
CotyStrumia <constanzastrumia28@gmail.com>
2024-07-14 19:18:53 -0300

    Finalizado

--- backend/clients/db_clients.go
+++ backend/clients/db_clients.go
@@ -3,6 +3,10 @@ package clients
 import (
 	"backend/model"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,25 +15,30 @@ var (
 	Db *gorm.DB
 )
 
-const (
-	dbUsername = "root"
-	dbPassword = "margoch"
-	dbHost     = "localhost"
-	dbPort     = 3306
-	dbSchema   = "dbarquisoft1" //
-	dsn        = "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
-)
-
 func ConnectDatabase() {
+	dbUsername := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbSchema := os.Getenv("DB_NAME")
+	dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+
 	connection := fmt.Sprintf(dsn, dbUsername, dbPassword, dbHost, dbPort, dbSchema)
+
 	var err error
-	Db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+	for i := 0; i < 10; i++ { // Intentar conectar hasta 10 veces
+		Db, err = gorm.Open(mysql.Open(connection), &gorm.Config{})
+		if err == nil {
+			fmt.Println("Connection Established")
+			break
+		}
+		fmt.Println("Error connecting to DB, retrying in 5 seconds...", err)
+		time.Sleep(5 * time.Second)
+	}
 
 	if err != nil {
-		fmt.Println("Error connecting to DB", err)
-		panic(err)
+		log.Fatalf("Could not connect to the database: %v", err)
 	}
-	fmt.Println("Connection Established")
 }
 
 func MigrateEntities() {
